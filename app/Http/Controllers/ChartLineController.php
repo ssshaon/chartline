d98df3475785ec135cacebfcb98c863ec841cc6f
Shinorina Shahrin Shaon <shaon.pn@gmail.com>
2017-05-12 23:42:30 +0600

    no view

--- app/Http/Controllers/ChartLineController.php
+++ app/Http/Controllers/ChartLineController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 class ChartLineController extends Controller
 {
     public function chartline(){
-        $devlist = chartline::table('charts')
-            ->select(chartline::raw('temperature as Temp'), DB::raw("DATE_FORMAT(created_at,'%Y-%m') as Date"), DB::raw('count(*) as temperatures'))
+        $devlist = charts::table('chartline')
+            ->select(charts::raw('temperature as Temp'), charts::raw("DATE_FORMAT(created_at,'%Y-%m') as Date"), charts::raw('count(*) as temperatures'))
             ->groupBy('Date')
             ->get();
 
---
+++ app/charts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shaon
+ * Date: 5/12/2017
+ * Time: 12:04 AM
+ */
+
+namespace App;
+
+
+class charts
+{
+
+}
